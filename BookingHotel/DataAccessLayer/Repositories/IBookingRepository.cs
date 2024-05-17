28e9020de3223be463c85c75b54445b93f7a7bf4
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2024-05-17 20:50:01 +0300

    Add Repositories

---
+++ BookingHotel/DataAccessLayer/Repositories/IBookingRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IBookingRepository
+    {
+        void Add(Booking booking);
+        void Update(Booking booking);
+        Booking GetById(int id);
+        IEnumerable<Booking> GetByGuestId(int guestId);
+        IEnumerable<Booking> GetByHotelId(int hotelId);
+    }
+}
---
+++ BookingHotel/DataAccessLayer/Repositories/IHotelRepository.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IHotelRepository
+    {
+        Hotel GetById(int hotelId);
+        IEnumerable<Hotel> GetAll();
+        void Add(Hotel hotel);
+        void Update(Hotel hotel);
+        void Delete(int hotelId);
+
+    }
+}
