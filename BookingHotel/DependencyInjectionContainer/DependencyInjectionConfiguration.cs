6d9a724946a3e3e27e95fc628ca9b51f4e91665b
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2024-05-29 19:56:56 +0300

    add DependencyInjectionConfiguration

--- BookingHotel/DependencyInjectionContainer/DependencyInjectionConfiguration.cs
+++ BookingHotel/DependencyInjectionContainer/DependencyInjectionConfiguration.cs
@@ -1,7 +1,32 @@
-﻿namespace DependencyInjectionContainer
-{
-    public class Class1
-    {
+﻿using DataAccessLayer.Context;
+using DataAccessLayer.Repositories;
+using BusinessLogicLayer.Interfaces;
+using BusinessLogicLayer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Repositories.Interfases;
+using DataAccessLayer.Repositories.Servises;
 
+
+namespace DependencyInjectionContainer
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static IServiceCollection AddHotelBookingServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<HotelBookingDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("HotelBookingDataBase")));
+            services.AddScoped<IHotelRepository, HotelRepository>();
+            services.AddScoped<IBookingRepository, BookingRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+            services.AddScoped<IGuestRepository, GuestRepository>();
+            services.AddScoped<IGuestService, GuestService>();
+            services.AddScoped<IRoomService, RoomService>();
+            services.AddScoped<INotificationService, EmailNotificationService>();
+            services.AddScoped<IPricing, StandardPrice>();
+            
+            return services;
+
+        }
     }
 }
