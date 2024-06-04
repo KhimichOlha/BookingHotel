9dca6845266ca80e33003dedc14e262b096358b7
KhimichOlha <146723745+KhimichOlha@users.noreply.github.com>
2024-06-04 21:31:00 +0300

    BookingViewModel

--- BookingHotel/PresentationLayer/Controllers/AccountController.cs
+++ BookingHotel/PresentationLayer/Controllers/AccountController.cs
@@ -17,14 +17,14 @@ namespace PresentationLayer.Controllers
             _guestService = guestService;
         }
 
-        
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
         [HttpPost]
-        public async IActionResult Register(RegisterViewModel userview)
+        public async Task<IActionResult> Register(RegisterViewModel userview)
         {
             if (ModelState.IsValid)
             {
@@ -34,7 +34,91 @@ namespace PresentationLayer.Controllers
                     UserName = userview.Email
                 };
                 var result = await _userManager.CreateAsync(user, userview.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    _guestService.Add(new DataAccessLayer.Models.Guest
+                    {
+                        Email = userview.Email,
+                        UserId = user.Id
+
+                    });
+                    return RedirectToAction("Index", "Booking");
+
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+
+                }
             }
+            return View(userview);
+
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                var result = await _signInManager.PasswordSignInAsync(login.Email, login.Password, login.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+
+                    return RedirectToAction("Index", "Booking");
+
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+
+
+            }
+            return View(login);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel changePassword)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return RedirectToAction("Login");
+
+                }
+                var result = await _userManager.ChangePasswordAsync(user, changePassword.OldPassword, changePassword.NewPassword);
+                if (result.Succeeded)
+                {
+                    await _signInManager.RefreshSignInAsync(user);
+                    return RedirectToAction("Index", "Booking");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(changePassword);
+            
         }
     }
 }
--- BookingHotel/PresentationLayer/Controllers/GuestController.cs
+++ BookingHotel/PresentationLayer/Controllers/GuestController.cs
@@ -1,12 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using BusinessLogicLayer.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Models;
 
 namespace PresentationLayer.Controllers
 {
     public class GuestController : Controller
     {
-        public IActionResult Index()
+        private readonly IGuestService _guestService;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IMapper _map;
+        public GuestController(IGuestService guestService, UserManager<IdentityUser> userManager, IMapper map)
         {
-            return View();
+            _guestService = guestService;
+            _userManager = userManager;
+            _map = map;
         }
+        [HttpGet]
+        public async Task<IActionResult> Edit()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Acount");
+
+            }
+            var guest = _guestService.GetByUserId(user.Id);
+            if (guest == null)
+            {
+                return NotFound();
+
+            }
+            var guestModel = _map.Map<GuestViewModel>(guest);
+            return View(guestModel);
+        }
+        [HttpPost]
+        
+        public async Task<IActionResult> Edit(GuestViewModel guestModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var guest = _guestService.GetByUserId(user.Id);
+                if (guest == null)
+                {
+                    return NotFound();
+                }
+                guest.Name = guestModel.Name;
+                guest.Phone = guestModel.Phone;
+                _guestService.Update(guest);
+                return RedirectToAction("Details", guest.Id);
+
+            }
+            return View(guestModel);
+
+        }
+        public IActionResult Details(int Id)
+        {
+            var guest = _guestService.GetById(Id);
+            if (guest == null)
+            {
+                return NotFound();
+            }
+            return View(_map.Map<GuestViewModel>(guest));
+            
+        }
+        public IActionResult BookingHistory(int Id)
+        {
+            var guest = _guestService.GetById(Id);
+            if (guest == null)
+            {
+                return NotFound();
+            }
+            var bookings = _guestService.GetBookingsForGuest(guest.Id);
+            if(bookings == null)
+            {
+                return NotFound();
+            }
+            var bookingModels = new List<BookingViewModel>();
+            foreach (var booking in bookings)
+            {
+                bookingModels.Add(_map.Map<BookingViewModel>(booking);
+            }
+            return View(bookingModels);
+        }
+
+
     }
 }
